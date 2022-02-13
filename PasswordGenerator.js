d60db3a9c2dece48d9847d468afcab5e21e950e7
Colton Bothe <coltonbothe@gmail.com>
2022-02-13 15:16:36 -0600

    Form Creation
    created form in html plus added links to js and css files, added background color to css file, created function in js file to get elements from form and print as an alert.

--- PasswordGenerator.js
+++ PasswordGenerator.js
@@ -2,3 +2,14 @@
     Password generator used for quick and easy password creation.
 */
 
+function getInputValue() {
+
+    //obtain values from form.
+    let passwordLength = document.getElementById("length").value;
+    let includeNumbers = document.getElementById("numbers").checked;
+    let includeSymbols = document.getElementById("symbols").checked;
+
+    //print out values to an alert.
+    alert(passwordLength+"\r"+includeNumbers+"\r"+includeSymbols);
+
+}
